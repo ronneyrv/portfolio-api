a02adaf13b044569d3013793bc915f4713a29be5
Ronney <ronneyrv@gmail.com>
2026-05-09 11:48:35 -0300

    chore: initial spring boot setup

---
+++ src/main/java/com/ronney/portfolioapi/PortfolioapiApplication.java
@@ -0,0 +1,13 @@
+package com.ronney.portfolioapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ronney/portfolioapi/PortfolioapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ronney.portfolioapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
